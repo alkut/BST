1df6f075b549ebe774c58215f23ebd5902e590a1
alex <alexseykutasov@gmail.com>
2023-04-19 00:36:22 +0500

    fast fix

--- cmake/EnableLinter.cmake
+++ cmake/EnableLinter.cmake
@@ -1,7 +1,10 @@
 add_custom_target(
         cpplint
         COMMAND
-        cpplint --recursive --exclude=build/* --output=emacs . > ./lint/analyze.txt 2> ./lint/analyze.txt && cat ./lint/analyze.txt
+        mkdir -p lint &&
+        cpplint --recursive --exclude=build/* --output=emacs . > ./lint/analyze.txt 2> ./lint/analyze.txt &&
+        cat ./lint/analyze.txt &&
+        rm -rf lint
         WORKING_DIRECTORY
         ${CMAKE_CURRENT_SOURCE_DIR}
         VERBATIM
